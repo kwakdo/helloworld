bc737453843af7ada96b677d57704a6a0982b6ff
kwakdo <vv05044@gmail.com>
2022-04-25 15:37:50 +0900

    commit

---
+++ src/main/resources/helloworld.java
@@ -0,0 +1,10 @@
+package com.douzone.helloworld;
+
+public class helloworld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+	}
+
+}
